9237a1ac931f127fead77d57671e136615a574fc
Sush2k04 <129118657+Sush2k04@users.noreply.github.com>
2024-05-11 16:22:24 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,39 @@
+def add(a,c):
+    return a+c
+def mul(a,c):
+    return a*c
+def div(a,c):
+    return a/c
+def sub(a,c):
+    return a-c
+def power(a,c):
+    return a**c
+print("=========================================")
+print("CALCULATOR")
+print("=========================================")
+
+choice=0
+while choice!=1:
+    
+    a=int(input("ENTER A NUMBER:"))
+    b=input("ENTER AN OPERATION(+,-,x,/,^):")
+    c=int(input("ENTER ANOTHER NUMBER:"))
+    if b=='+':
+        print(add(a,c))
+    elif b=='-':
+        print(sub(a,c))
+    elif b=='/':
+        print(div(a,c))
+    elif b=='x':
+        print(mul(a,c))
+    elif b=='^':
+        print(power(a,c))
+    print("=========================================")
+    d=input("AGAIN?(y/n):")
+    if d=='y':
+        choice=0
+    else:
+        choice=1
+        print("=========================================")
+
+
